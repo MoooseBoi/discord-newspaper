d003be07c61ae0e2c4dfbe6b8832c595eb0865ef
moose <mooooseboi@gmail.com>
2024-01-17 18:32:03 +0200

    added schedule task to send feed every day

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ load_dotenv()
 
 hn_pattern = re.compile(r'<p>Points: (\d+)</p>')
 
+
 def get_hn_embed(query=""):
     url = "https://hnrss.org/frontpage"
     if query != "":
@@ -38,14 +39,6 @@ def get_hn_embed(query=""):
 
 
 def main():
-    # track urls in json files
-    # command for adding, listing and removing urls
-
-    # add integration with different feed types:
-    # - reddit
-    # - hacker news
-    # - add more here :3
-
     intents = discord.Intents.default()
     intents.message_content = True
     bot = commands.Bot(command_prefix="!", intents=intents)
@@ -53,16 +46,12 @@ def main():
     @bot.event
     async def on_ready():
         print(f"{bot.user} online")
+        daily_feed.start()
 
-    @bot.command(name="feed")
-    async def feed(context, *args):
-        # experimental command to test feed
+    @tasks.loop(time=datetime.time(hour=12, tzinfo=datetime.timezone.utc))
+    async def daily_feed():
         news_channel = bot.get_channel(int(os.getenv("NEWS_CH_ID")))
         await news_channel.send(embed=get_hn_embed())
-
-    @tasks.loop(time=datetime.time(hour=8, minute=0))
-    async def daily_feed():
-        pass
 
     bot.run(os.getenv("TOKEN"))
 
