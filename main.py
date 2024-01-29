9c7c15f3f14bca7ebbe8d5850f305b5146ad3aa0
vorkutavorkutlag <royzurakipiani@gmail.com>
2024-01-29 16:40:44 +0200

    Fixed feed functions & polished creation, removal, updating functions

--- main.py
+++ main.py
@@ -3,30 +3,18 @@ import datetime
 import discord
 from discord.ext import commands, tasks
 import importlib
+import handlers
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-
-news_channels = set()
-
-
-def get_feed_embeds():
-    handles = os.listdir("handles")
-    embeds = []
-
-    for handle in handles:
-        module = f"handles.{handle.replace('.py', '')}"
-        try:
-            embed = importlib.import_module(module).get_embed()
-            embeds.append(embed)
-        except AttributeError:
-            continue
-
-    return embeds
+channels = {}
 
 
 def main():
+    global channels
+
     intents = discord.Intents.default()
     intents.message_content = True
     bot = commands.Bot(command_prefix="n!", intents=intents)
@@ -36,30 +24,121 @@ def main():
         print(f"{bot.user} online")
         daily_feed.start()
 
-    @bot.command(name="add")
-    async def add_channel(ctx):
-        news_channels.add(ctx.channel.id)
-        print("added ", ctx.channel.id)
+    @bot.command(name="init")
+    async def init(ctx):
+        # consider: should this function even exist?
 
-    @bot.command(name="rmv")
-    async def remove_channel(ctx):
-        news_channels.remove(ctx.channel.id)
+        # get channel id
+        # add id to channels dict with an empty entry
+        # Exception: channel exists => do nothing? send error message?
+
+        embed = discord.Embed().add_field(name="Error", value=f"#{ctx.channel.name} already subscribed!")
+        if ctx.channel.id not in channels.keys():
+            channels[ctx.channel.id] = {}
+            embed = discord.Embed().add_field(name="Salutations", value=f"Hello, #{ctx.channel.name}!")
+        await ctx.channel.send(embed=embed)
+
+    @bot.command(name="exit")
+    async def exit(ctx):
+        try:
+            del channels[ctx.channel.id]
+            embed = discord.Embed().add_field(name="Farewell", value=f"Goodbye, #{ctx.channel.name}!")
+        except KeyError:
+            embed = discord.Embed().add_field(name="Error", value=f"#{ctx.channel.name} is not subscribed.")
+        finally:
+            await ctx.channel.send(embed=embed)
+
+    @bot.command(name="add")
+    async def add(ctx, *args):
+        # get channel id
+        # get dictionary associated with id
+        # check args[0] for existing handle
+        # send result embed
+        # Exception: Unsupported handler => wrong handler message
+        # Exception: wrong args => args error message
+        try:
+            if not handlers.handle_exists(args[0]):
+                embed = discord.Embed().add_field(name="Unsupported Handler",
+                                                  value=f"{args[0]}'s API is unsupported.")
+                await ctx.channel.send(embed=embed)
+                return
+
+            handler = importlib.import_module(f"handlers.{args[0]}")
+
+            if not handler.verify_args(args):
+                embed = discord.Embed().add_field(name="Bad Argument",
+                                                  value=f"Bad argument for {args[0]}!")
+                await ctx.channel.send(embed=embed)
+                return
+
+            if len(args) == 1:
+                channels[ctx.channel.id][args[0]] = True
+                embed = discord.Embed().add_field(
+                    name="Success", value=f"Succesfully subscribed to {args[0]}")
+
+            else:
+
+                if args[0] in channels[ctx.channel.id].keys():
+                    channels[ctx.channel.id][args[0]].add(args[1])
+                else:
+                    channels[ctx.channel.id][args[0]] = {args[1]}
+                embed = discord.Embed().add_field(
+                    name="Success", value=f"Subscribed to {args[0]}'s {args[1]}")
+
+        except KeyError or IndexError:
+            embed = discord.Embed().add_field(
+                name="Error Occurred",
+                value="Couldn't subscribe. Are you sure you initialised? ('n!init') | "
+                      "Write 'n!help' for available commands.")
+
+        await ctx.channel.send(embed=embed)
+
+    @bot.command(name="remove")  # Unsubscribe
+    async def remove(ctx, *args):
+        try:
+            # If this throws an exception, it means user entered 'n!remove <name>'
+            channels[ctx.channel.id][args[0]].remove(args[1])
+
+            if channels[ctx.channel.id][args[0]] == set():
+                del channels[ctx.channel.id][args[0]]
+
+            embed = discord.Embed().add_field(name="Success", value=f"Succesfully unsubscribed from {args[0]}'s {args[1]}")
+
+        except (IndexError, AttributeError):
+            del channels[ctx.channel.id][args[0]]
+            embed = discord.Embed().add_field(name="Success", value=f"Succesfully unsubscribed from {args[0]}")
+
+        except KeyError:
+            embed = discord.Embed().add_field(
+                name="Error Occurred", value="You are not subscribed to that. Write 'n!help' for available commands.")
+
+        await ctx.channel.send(embed=embed)
+
+    @bot.command(name="list-feeds")
+    async def list_feeds(ctx):  # send list of subscribed feeds
+        embed = discord.Embed(title=f"__Subscribed Feeds__", color=0x4f86c9)
+        try:
+            for sub in channels[ctx.channel.id].keys():
+                embed.add_field(inline=False, name=sub, value=f"{sub} Subscriptions: {(channels[ctx.channel.id][sub])}")
+        except KeyError:
+            embed.add_field(inline=False, name="Error!", value="You are not subscribed to anything yet.")
+        finally:
+            await ctx.channel.send(embed=embed)
+
+    # TO DO - CONFIGURE daily_feed_commands() AND daily_feed() TO ONLY SEND THE STUFF WE SUBSCRIBED TO
 
     @bot.command(name="feed")
     async def daily_feed_command(ctx):
-        embeds = get_feed_embeds()
-
+        embeds = handlers.get_embeds(channels[ctx.channel.id])
         for embed in embeds:
             await ctx.channel.send(embed=embed)
 
     @tasks.loop(time=datetime.time(hour=12, tzinfo=datetime.timezone.utc))
     async def daily_feed():
-        embeds = get_feed_embeds()
 
-        for embed in embeds:
-            # initialize channels seperately (lazy load?)
-            for id in news_channels:
-                channel = bot.get_channel(id)
+        for channel in channels.keys(): # initialize channels seperately (lazy load?)
+            embeds = handlers.get_embeds(channels[channel.id])
+            for embed in embeds:
                 await channel.send(embed=embed)
 
     bot.run(os.getenv("TOKEN"))
